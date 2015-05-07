b8c1a4a3c7c1421057d7d503260885a81e34132f
Malte Klemke <git@malde.me>
2015-05-07 16:03:25 +0200

    zpretzorc added to shellscript

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,7 +8,7 @@
 
 dir=~/.dotfiles                    # dotfiles directory
 olddir=~/.dotfiles_old/             # old dotfiles backup directory
-files=".zshrc .vimrc .gitconfig .gitignore_global"    # list of files/folders to symlink in homedir
+files=".zshrc .zpreztorc .vimrc .gitconfig .gitignore_global"    # list of files/folders to symlink in homedir
 
 ##########
 
